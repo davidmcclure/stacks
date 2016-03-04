e6bd9c0f026adc9fd10235465ec1768ab998d339
David McClure <davidwilliammcclure@gmail.com>
2016-03-04 10:50:52 -0800

    Add pip dependencies.

---
+++ stacks/settings/production.py
@@ -0,0 +1,15 @@
+
+
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stacks',
+        'USER': 'stacks',
+        'PASSWORD': '4wGnfZt4ZLaYa%z2',
+    }
+}
+
+STATIC_ROOT = '/opt/stacks/static'
