e112664010bfebfee0c5921ae0d962b34e49a538
David McClure <davidwilliammcclure@gmail.com>
2016-03-04 09:52:36 -0800

    Always use the production settings.

--- stacks/wsgi.py
+++ stacks/wsgi.py
@@ -1,5 +1,6 @@
+
 """
-WSGI config for stacks project.
+WSGI config for textbase project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +12,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stacks.settings")
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'stacks.settings.production',
+)
 
 application = get_wsgi_application()
