566eea4db04f9ee351c5db31d0950f6657afda9f
David McClure <davidwilliammcclure@gmail.com>
2016-03-07 19:19:17 -0800

    Add a to the corpus factory.

--- corpora/factories/corpus.py
+++ corpora/factories/corpus.py
@@ -15,3 +15,7 @@ class CorpusFactory(factory.DjangoModelFactory):
     name = factory.Sequence(
         lambda n: 'Corpus {0}'.format(n)
     )
+
+    slug = factory.Sequence(
+        lambda n: 'corpus-{0}'.format(n)
+    )
