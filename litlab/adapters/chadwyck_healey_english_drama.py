6bea7d6866be3930634e0998f6e8a62bc404668d
David McClure <davidwilliammcclure@gmail.com>
2016-03-07 18:29:18 -0800

    Block in source_text, plain_text, title, and author properties on the CH english drama text adapter.

--- litlab/adapters/chadwyck_healey_english_drama.py
+++ litlab/adapters/chadwyck_healey_english_drama.py
@@ -5,6 +5,7 @@ import glob
 
 from bs4 import BeautifulSoup
 
+from litlab.utils import get_text
 from litlab.conf import settings
 
 
@@ -64,3 +65,51 @@ class Text:
 
         with open(self.path, 'rb') as fh:
             self.xml = BeautifulSoup(fh, 'lxml')
+
+
+    @property
+    def source_text(self):
+
+        """
+        Get the raw markup as a string.
+
+        Returns: str
+        """
+
+        return str(self.xml)
+
+
+    @property
+    def plain_text(self):
+
+        """
+        Extract plaintext.
+
+        Returns: str
+        """
+
+        return get_text(self.xml, 'play')
+
+
+    @property
+    def title(self):
+
+        """
+        Query the title.
+
+        Returns: str
+        """
+
+        return get_text(self.xml, 'voltitle')
+
+
+    @property
+    def author(self):
+
+        """
+        Query the author.
+
+        Returns: str
+        """
+
+        return get_text(self.xml, 'volauth')
