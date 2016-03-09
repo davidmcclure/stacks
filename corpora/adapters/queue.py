263fbdcc34a1912a095258b1ff85587f415cf817
David McClure <davidwilliammcclure@gmail.com>
2016-03-09 13:21:47 -0800

    Stub in a abstract QueueAdapter class.

---
+++ corpora/adapters/queue.py
@@ -0,0 +1,49 @@
+
+
+import django_rq
+
+from corpora.models import Corpus, Text
+
+
+class QueueAdapter:
+
+
+    @property
+    def name(self):
+        raise NotImplementedError
+
+
+    @property
+    def slug(self):
+        raise NotImplementedError
+
+
+    @classmethod
+    def job(self, corpus_id, path):
+        raise NotImplementedError
+
+
+    def paths(self):
+        raise NotImplementedError
+
+
+    def queue(self):
+
+        """
+        Queue accessioning jobs in RQ.
+        """
+
+        # Delete the existing corpus.
+        Corpus.objects.filter(slug=self.slug).delete()
+
+        # Create a new corpus.
+        corpus = Corpus.objects.create(
+            name=self.name,
+            slug=self.slug,
+        )
+
+        queue = django_rq.get_queue()
+
+        # Spool a job for each source.
+        for path in self.paths():
+            queue.enqueue(self.__class__.job, corpus.id, path)
