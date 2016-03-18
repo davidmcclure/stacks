629121ce27fa3fef0826448504be7e087be37ee9
David McClure <davidwilliammcclure@gmail.com>
2016-03-18 10:43:45 -0700

    Use the lxml-xml parser for the Chadwyck Healey parsers, not lxml, which is actually a HTML parser.

--- litlab/adapters/chadwyck_healey_early_american_fiction/source.py
+++ litlab/adapters/chadwyck_healey_early_american_fiction/source.py
@@ -22,7 +22,7 @@ class Source:
         self.path = os.path.abspath(path)
 
         with open(self.path, 'rb') as fh:
-            self.xml = BeautifulSoup(fh, 'lxml')
+            self.xml = BeautifulSoup(fh, 'lxml-xml')
 
 
     def volumes(self):
