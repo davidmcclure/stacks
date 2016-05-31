f87de8fda97cde6182269fcd5954de9723f5ef77
David McClure <davidwilliammcclure@gmail.com>
2016-05-31 13:22:24 -0700

    Load testing cases from a YAML file.

--- corpus/tests/adapters/chadwyck_healey_american_drama/test_ingest.py
+++ corpus/tests/adapters/chadwyck_healey_american_drama/test_ingest.py
@@ -3,6 +3,7 @@
 import os
 import django_rq
 import pytest
+import yaml
 
 from django.core.management import call_command
 from corpus.adapters import ChadwyckHealeyAmericanDrama
@@ -15,38 +16,23 @@ pytestmark = [
 ]
 
 
-@pytest.mark.parametrize('query', [
+def test_ingest(settings):
 
-    dict(
-
-        title   = 'Favelle: Or The Fatal Duel (1809)',
-        author  = 'Adams, C. L. (Charles L.)',
-        year    = 1809,
-
-        plain_text__icontains = 'it is possible that an unguarded word may',
-
-    ),
-
-    dict(
-
-        title   = 'Vanity; or, A Lord in Philadelphia (1854)',
-        author  = 'Addis, J. B.',
-        year    = 1854,
-
-        plain_text__icontains = 'Of course, I sent a liveried servant',
-
-    ),
-
-])
-def test_ingest(query, settings):
+    dirname = os.path.dirname(__file__)
 
     # Inject fixtures.
     settings.CORPUS_CHADWYCK_HEALEY_AMERICAN_DRAMA = os.path.join(
-        os.path.dirname(__file__), 'fixtures',
+        dirname, 'fixtures',
     )
 
     # Run the ingest.
     call_command('queue_ingest', 'ChadwyckHealeyAmericanDrama')
     django_rq.get_worker().work(burst=True)
 
-    assert Text.objects.filter(**query).exists()
+    # Check for texts.
+    with open(os.path.join(dirname, 'texts.yml')) as fh:
+
+        texts = yaml.load(fh)
+
+        for text in texts:
+            assert Text.objects.filter(**text).exists()
