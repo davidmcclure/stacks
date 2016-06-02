3a890694b004aeaae75ff4ed7b48813ff206c147
David McClure <davidwilliammcclure@gmail.com>
2016-06-02 13:25:20 -0700

    Parametrize the YAML tests.

--- corpus/tests/adapters/chadwyck_healey_american_poetry/test_ingest.py
+++ corpus/tests/adapters/chadwyck_healey_american_poetry/test_ingest.py
@@ -16,15 +16,26 @@ pytestmark = [
 ]
 
 
-def test_ingest(settings):
+def pytest_generate_tests(metafunc):
 
-    dirname = os.path.dirname(__file__)
+    """
+    Inject the test cases.
+    """
 
-    fixtures_path = os.path.join(dirname, 'fixtures')
-    text_path = os.path.join(dirname, 'texts.yml')
+    path = os.path.join(os.path.dirname(__file__), 'texts.yml')
 
-    # Inject fixtures.
-    settings.CORPUS_CHADWYCK_HEALEY_AMERICAN_POETRY = fixtures_path
+    with open(path) as fh:
+        tests = yaml.load(fh)
+
+    metafunc.parametrize('id,fields', tests.items())
+
+
+def test_ingest(settings, id, fields):
+
+    path = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+    # Patch the corpus path.
+    settings.CORPUS_CHADWYCK_HEALEY_AMERICAN_POETRY = path
 
     # Run the ingest.
     call_command('queue_ingest', 'ChadwyckHealeyAmericanPoetry')
@@ -32,17 +43,12 @@ def test_ingest(settings):
 
     corpus = Corpus.objects.get(slug='chadwyck-healey-american-poetry')
 
-    # Read the YAML cases.
-    with open(text_path) as fh:
-        texts = yaml.load(fh)
+    # Check for text.
 
-    # Check for texts.
-    for id, fields in texts.items():
+    text = Text.objects.get(corpus=corpus, identifier=id)
 
-        text = Text.objects.get(corpus=corpus, identifier=id)
+    for key, val in fields.get('equals', {}).items():
+        assert getattr(text, key) == val
 
-        for key, val in fields.get('equals', {}).items():
-            assert getattr(text, key) == val
-
-        for key, val in fields.get('contains', {}).items():
-            assert val in getattr(text, key)
+    for key, val in fields.get('contains', {}).items():
+        assert val in getattr(text, key)
