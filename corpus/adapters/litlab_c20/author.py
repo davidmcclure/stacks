e66a72a06d0176c512942c7b2388712697b0f298
David McClure <davidwilliammcclure@gmail.com>
2016-06-07 14:22:49 -0700

    Block in an Author class for litlab C20.

---
+++ corpus/adapters/litlab_c20/author.py
@@ -0,0 +1,56 @@
+
+
+import os
+
+from configobj import ConfigObj
+
+
+class Author:
+
+
+    def __init__(self, path):
+
+        """
+        Canonicalize the author path.
+
+        Args:
+            path (str)
+        """
+
+        self.path = os.path.abspath(path)
+
+
+    @property
+    def metadata_path(self):
+
+        """
+        Get the path of the author's metadata file.
+
+        Returns: str
+        """
+
+        return os.path.join(self.path, 'metadata.txt')
+
+
+    @property
+    def metadata(self):
+
+        """
+        Parse the metadata file.
+
+        Returns: dict
+        """
+
+        return dict(ConfigObj(self.metadata_path))
+
+
+    @property
+    def name_full(self):
+
+        """
+        Get the author's full name as a single string.
+
+        Returns: str
+        """
+
+        return ', '.join(self.metadata['name_full'])
