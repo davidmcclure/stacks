3f01f8cc794285b79527847db3c8a9678e3cb4a1
David McClure <davidwilliammcclure@gmail.com>
2016-06-22 09:59:13 -0700

    Get rid of the stub interface URL registration.

--- stacks/urls.py
+++ stacks/urls.py
@@ -7,5 +7,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^queue/', include('django_rq.urls')),
-    url(r'^', include('interface.urls')),
 ]
