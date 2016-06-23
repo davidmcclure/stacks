25cd7555048caca99cea6b117fd53222e81caa09
David McClure <davidwilliammcclure@gmail.com>
2016-06-23 16:47:25 -0700

    Stub in a test runner for Corpus.ingest().

--- corpus/tests/adapters/litlab_c20/corpus/test_ingest.py
+++ corpus/tests/adapters/litlab_c20/corpus/test_ingest.py
@@ -1,4 +1,22 @@
 
 
+import pytest
+import django_rq
+
+from corpus.models import Corpus, Text
+
+
+pytestmark = [
+    pytest.mark.django_db,
+    pytest.mark.usefixtures('redis'),
+]
+
+
 def test_test(corpus):
-    assert True
+
+    corpus.ingest()
+
+    django_rq.get_worker().work(burst=True)
+
+    assert Corpus.objects.count() == 1
+    assert Text.objects.count() == 5
