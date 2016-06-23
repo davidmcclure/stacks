771063e82ca5520b013e095bbd64c70de2604ba9
David McClure <davidwilliammcclure@gmail.com>
2016-06-23 16:06:02 -0700

    Test Text#title().

--- corpus/tests/adapters/litlab_c20/author/test_name_full.py
+++ corpus/tests/adapters/litlab_c20/author/test_name_full.py
@@ -1,14 +1,10 @@
 
 
-import pytest
-
 from corpus.adapters.litlab_c20.author import Author
 
 
-@pytest.fixture
-def stephen_king(get_author):
-    return get_author('King, Stephen')
+def test_name_full(get_author):
 
+    stephen_king = get_author('King, Stephen')
 
-def test_name_full(stephen_king):
     assert stephen_king.name_full() == 'King, Stephen'
