796baa11327b282529e1e5f485680e263226c561
David McClure <davidwilliammcclure@gmail.com>
2016-06-23 16:06:02 -0700

    Test Text#title().

--- corpus/tests/adapters/litlab_c20/conftest.py
+++ corpus/tests/adapters/litlab_c20/conftest.py
@@ -4,6 +4,7 @@ import pytest
 import os
 
 from corpus.adapters.litlab_c20.author import Author
+from corpus.adapters.litlab_c20.text import Text
 
 
 @pytest.fixture()
@@ -13,13 +14,32 @@ def get_author():
     Wrap an author fixture.
     """
 
-    def _get_author(name):
+    def _get_author(author_name):
 
         path = os.path.join(
             os.path.dirname(__file__),
-            'fixtures', name,
+            'fixtures', author_name,
         )
 
         return Author(path)
 
     return _get_author
+
+
+@pytest.fixture()
+def get_text():
+
+    """
+    Wrap a text fixture.
+    """
+
+    def _get_text(author_name, text_name):
+
+        path = os.path.join(
+            os.path.dirname(__file__), 'fixtures',
+            author_name, text_name,
+        )
+
+        return Text(path)
+
+    return _get_text
