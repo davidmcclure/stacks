7607c60b672ac24a211c1d2b45e6f82fec755828
David McClure <davidwilliammcclure@gmail.com>
2016-06-24 15:26:17 -0700

    Get rid of the unused get_author and get_text fixtures.

--- corpus/tests/adapters/litlab_c20/conftest.py
+++ corpus/tests/adapters/litlab_c20/conftest.py
@@ -5,46 +5,6 @@ import os
 import django_rq
 
 from corpus.adapters.litlab_c20.corpus import Corpus
-from corpus.adapters.litlab_c20.author import Author
-from corpus.adapters.litlab_c20.text import Text
-
-
-@pytest.fixture()
-def get_author():
-
-    """
-    Wrap an author fixture.
-    """
-
-    def _get_author(author_name):
-
-        path = os.path.join(
-            os.path.dirname(__file__),
-            'fixtures', author_name,
-        )
-
-        return Author(path)
-
-    return _get_author
-
-
-@pytest.fixture()
-def get_text():
-
-    """
-    Wrap a text fixture.
-    """
-
-    def _get_text(author_name, text_name):
-
-        path = os.path.join(
-            os.path.dirname(__file__),
-            'fixtures', author_name, text_name,
-        )
-
-        return Text(path)
-
-    return _get_text
 
 
 @pytest.fixture(scope='module')
