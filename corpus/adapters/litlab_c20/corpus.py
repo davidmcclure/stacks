27cd16fb4c9075526b2c69d14c21b6095ac6b0c7
David McClure <davidwilliammcclure@gmail.com>
2016-06-27 10:34:21 -0700

    Get rid of the base Corpus class in litlab.

--- corpus/adapters/litlab_c20/corpus.py
+++ corpus/adapters/litlab_c20/corpus.py
@@ -1,11 +1,14 @@
 
 
+import os
+
+from django.conf import settings
+
 from corpus.models import Corpus as StacksCorpus
-from corpus.adapters.litlab.corpus import Corpus as LitLabCorpus
 from corpus.adapters.litlab.jobs import ingest
 
 
-class Corpus(LitLabCorpus):
+class Corpus:
 
 
     @classmethod
@@ -18,6 +21,37 @@ class Corpus(LitLabCorpus):
         """
 
         return cls(settings.CORPUS_LITLAB_C20)
+
+
+    def __init__(self, path):
+
+        """
+        Canonicalize the corpus path.
+
+        Args:
+            path (str)
+        """
+
+        self.path = os.path.abspath(path)
+
+
+    def text_paths(self):
+
+        """
+        Generate a path for each text directory.
+
+        Yields: str
+        """
+
+        for author_dir in next(os.walk(self.path))[1]:
+
+            # Get the full author path.
+            author_path = os.path.join(self.path, author_dir)
+
+            for text_dir in next(os.walk(author_path))[1]:
+
+                # Yield each text path.
+                yield os.path.join(author_path, text_dir)
 
 
     def ingest(self):
