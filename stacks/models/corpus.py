a6fd83304dd37739aa15d76fbe866ae4c9fb3b0b
David McClure <davidwilliammcclure@gmail.com>
2016-06-29 14:21:21 -0700

    Make 'slug' on Corpus unique.

--- stacks/models/corpus.py
+++ stacks/models/corpus.py
@@ -1,6 +1,6 @@
 
 
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, UniqueConstraint
 
 from .base import Base
 
@@ -8,6 +8,10 @@ from .base import Base
 class Corpus(Base):
 
     __tablename__ = 'corpus'
+
+    __table_args__ = (
+        UniqueConstraint('slug'),
+    )
 
     name = Column(String, nullable=False)
 
