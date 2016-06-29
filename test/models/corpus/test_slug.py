2d5aba0b41f5dac583396ff934595f3872586909
David McClure <davidwilliammcclure@gmail.com>
2016-06-29 15:34:44 -0700

    Use a scoped session.

--- test/models/corpus/test_slug.py
+++ test/models/corpus/test_slug.py
@@ -16,12 +16,13 @@ def test_unique(config):
     Block duplicate slugs.
     """
 
-    with config.get_session() as session:
+    c1 = Corpus(name='Corpus 1', slug='slug')
+    c2 = Corpus(name='Corpus 1', slug='slug')
 
-        c1 = Corpus(name='Corpus 1', slug='slug')
-        c2 = Corpus(name='Corpus 1', slug='slug')
+    config.Session.add(c1)
+    config.Session.add(c2)
 
-        session.add(c1)
-        session.add(c2)
+    with pytest.raises(IntegrityError) as e:
+        config.Session.commit()
 
-        session.commit()
+    assert 'corpus_slug_key' in str(e)
