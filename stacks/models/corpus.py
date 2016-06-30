bff89262225941fd091ec55fb38fe91bcc62638c
David McClure <davidwilliammcclure@gmail.com>
2016-06-30 09:57:47 -0700

    Validate the slug format.

--- stacks/models/corpus.py
+++ stacks/models/corpus.py
@@ -1,5 +1,7 @@
 
 
+import re
+
 from sqlalchemy import Column, String, UniqueConstraint
 from sqlalchemy.orm import validates
 
@@ -7,6 +9,7 @@ from .base import Base
 
 
 class Corpus(Base):
+
 
     __tablename__ = 'corpus'
 
@@ -17,3 +20,25 @@ class Corpus(Base):
     name = Column(String, nullable=False)
 
     slug = Column(String, nullable=False)
+
+
+    @validates('slug')
+    def validate_slug(self, key, val):
+
+        """
+        Check the slug format.
+
+        Args:
+            key (str)
+            val (mixed)
+
+        Raises: AssertionError
+        """
+
+        if type(val) is str:
+
+            assert re.match('^[a-z0-9-]+$', val), (
+                'Slugs can only contain letters, numbers, and underscores.'
+            )
+
+        return val
--- test/models/corpus/test_slug.py
+++ test/models/corpus/test_slug.py
@@ -40,3 +40,42 @@ def test_unique():
         session.commit()
 
     assert 'corpus_slug_key' in str(e)
+
+
+@pytest.mark.parametrize('slug', [
+
+    # letters
+    'hathi',
+
+    # dashes
+    'hathi-trust',
+
+    # numbers
+    'hathi-trust-2016',
+
+])
+def test_valid(slug):
+    CorpusFactory(slug=slug)
+
+
+@pytest.mark.parametrize('slug', [
+
+    # capitals
+    'Hathi-Trust',
+
+    # underscores
+    'hathi_trust',
+
+    # spaces
+    'hathi trust',
+
+    # padding
+    ' hathi-trust ',
+
+    # punctuation
+    'hathi-trust!',
+
+])
+def test_invalid(slug):
+    with pytest.raises(AssertionError):
+        CorpusFactory(slug=slug)
