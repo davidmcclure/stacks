d561c5619d8354d0ab2372c0e2b4fca1faa53bcb
David McClure <davidwilliammcclure@gmail.com>
2016-06-30 14:37:38 -0700

    Rename common -> core.

--- test/models/corpus/test_slug.py
+++ test/models/corpus/test_slug.py
@@ -4,7 +4,7 @@ import pytest
 
 from sqlalchemy.exc import IntegrityError
 
-from stacks.common import session
+from stacks.core import session
 from stacks.models import Corpus
 
 from test.factories import CorpusFactory
