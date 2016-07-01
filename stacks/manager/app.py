6f740b2c1d66c1569dd3f3e86461ba1c1da6c45b
David McClure <davidwilliammcclure@gmail.com>
2016-07-01 16:26:36 -0700

    Break out separate __main__ files.

--- stacks/manager/app.py
+++ stacks/manager/app.py
@@ -11,7 +11,3 @@ app = Flask(__name__)
 @app.route('/')
 def stacks():
     return 'Stacks'
-
-
-if __name__ == '__main__':
-    app.run(port=os.getenv('PORT', 5000))
--- stacks/queue/app.py
+++ stacks/queue/app.py
@@ -8,9 +8,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config.from_object(rq_dashboard.default_settings)
 app.register_blueprint(rq_dashboard.blueprint, url_prefix='/rq')
-
-
-if __name__ == '__main__':
-    app.run(port=os.getenv('PORT', 5000))
+app.config.from_object(rq_dashboard.default_settings)
