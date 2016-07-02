84f1ac49fbea6460446962d6182037dcb9934eb3
David McClure <davidwilliammcclure@gmail.com>
2016-07-02 13:43:25 -0700

    Add a ECCO Text class.

---
+++ corpus/adapters/ecco/text.py
@@ -0,0 +1,60 @@
+
+
+import os
+
+from bs4 import BeautifulSoup
+
+from corpus.utils import get_text
+
+
+class Text:
+
+    def __init__(self, path):
+
+        """
+        Parse the XML.
+
+        Args:
+            path (str)
+        """
+
+        self.path = os.path.abspath(path)
+
+        with open(self.path, 'rb') as fh:
+            self.xml = BeautifulSoup(fh, 'xml')
+
+    def identifier(self):
+
+        """
+        Returns: str
+        """
+
+        return get_text(self.xml, 'documentID')
+
+    def author_marc_name(self):
+
+        """
+        Returns: str
+        """
+
+        return get_text(self.xml, 'author marcName')
+
+    def year(self):
+
+        """
+        Returns: int
+        """
+
+        return int(get_text(self.xml, 'pubDate')[:4])
+
+    def plain_text(self):
+
+        """
+        Returns: str
+        """
+
+        words = self.xml.select('wd')
+
+        strings = [w.string for w in words]
+
+        return ' '.join(strings)
