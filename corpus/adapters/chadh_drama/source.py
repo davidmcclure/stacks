a11abb5b3d9fe93e0d420e429fdffc6c519b22eb
David McClure <davidwilliammcclure@gmail.com>
2016-07-04 14:17:42 -0700

    Initial passing test for CH drama.

---
+++ corpus/adapters/chadh_drama/source.py
@@ -0,0 +1,35 @@
+
+
+import os
+
+from bs4 import BeautifulSoup
+
+from .play import Play
+
+
+class Source:
+
+    def __init__(self, path):
+
+        """
+        Parse the XML.
+
+        Args:
+            path (str): The text path.
+        """
+
+        self.path = os.path.abspath(path)
+
+        with open(self.path, 'rb') as fh:
+            self.xml = BeautifulSoup(fh, 'lxml')
+
+    def plays(self):
+
+        """
+        Generate Poem instances.
+
+        Yields: Poem
+        """
+
+        for tree in self.xml.find_all('div0'):
+            yield Play(tree)
