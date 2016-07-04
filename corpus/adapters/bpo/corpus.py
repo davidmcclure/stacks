d489607e9e9770acd4645688495a0385274d1852
David McClure <davidwilliammcclure@gmail.com>
2016-07-03 21:39:18 -0700

    Implement the BPO XML source discovery.

--- corpus/adapters/bpo/corpus.py
+++ corpus/adapters/bpo/corpus.py
@@ -1,6 +1,9 @@
 
 
 import os
+import scandir
+
+from zipfile import ZipFile
 
 from django.conf import settings
 
@@ -26,3 +29,31 @@ class Corpus:
         """
 
         self.path = os.path.abspath(path)
+
+    def zip_paths(self):
+
+        """
+        Get paths to each of the .zip archives.
+
+        Yields: str
+        """
+
+        for root, dirs, files in scandir.walk(self.path):
+            for name in files:
+
+                # Match .xml files.
+                if os.path.splitext(name)[1] == '.zip':
+                    yield os.path.join(root, name)
+
+    def xml_paths(self):
+
+        """
+        Get (archive, path) tuples for each of the compressed XML sources.
+
+        Yields: (str, str)
+        """
+
+        for zpath in self.zip_paths():
+            with ZipFile(zpath) as archive:
+                for name in archive.namelist():
+                    yield (zpath, name)
