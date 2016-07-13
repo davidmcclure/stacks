cc9beaff746894aa4ff7e0746717b7456c810e0c
David McClure <davidwilliammcclure@gmail.com>
2016-07-13 16:18:09 -0700

    Stub in the BPO article wrapper.

--- stacks/corpus/utils.py
+++ stacks/corpus/utils.py
@@ -10,8 +10,8 @@ def scan_paths(root, pattern):
     Walk a directory and gather up all file paths with a given extension.
 
     Args:
-        root (str)
-        ext (str)
+        root (str): The top-level directory.
+        pattern (str): A filename regex.
 
     Yields: str
     """
@@ -24,3 +24,25 @@ def scan_paths(root, pattern):
             # Match the extension.
             if pattern.search(name):
                 yield os.path.join(root, name)
+
+
+def get_text(tree, selector):
+
+    """
+    Extract text from an element. Return None if the element is missing or the
+    value is empty.
+
+    Args:
+        tree (BeautifulSoup): A bs4 tree.
+        selector (str): A CSS selector.
+
+    Returns: str|None
+    """
+
+    tag = tree.select_one(selector)
+
+    if tag:
+        return ' '.join(tag.stripped_strings) or None
+
+    else:
+        return None
