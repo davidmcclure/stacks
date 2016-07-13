752df7b43a0cb439c9b0e56c312c1425e44f5069
David McClure <davidwilliammcclure@gmail.com>
2016-07-13 16:13:54 -0700

    Failing test for the BPO cases.

--- test/corpus/adapters/bpo/corpus/test_ingest.py
+++ test/corpus/adapters/bpo/corpus/test_ingest.py
@@ -2,6 +2,9 @@
 
 import pytest
 
+from stacks.common.singletons import session
+from stacks.corpus.models import Text
+
 from test.utils import read_yaml
 
 
@@ -13,4 +16,29 @@ cases = read_yaml(__file__, 'ingest.yml')
 
 @pytest.mark.parametrize('identifier,fields', cases.items())
 def test_ingest(identifier, fields):
-    assert True
+
+    # TODO: get_by_identifer()
+    text = (
+        session
+        .query(Text)
+        .filter(Text.identifier==identifier)
+        .one()
+    )
+
+    if 'title' in fields:
+        assert text.title == fields['title']
+
+    if 'author_name_full' in fields:
+        assert text.author_name_full == fields['author_name_full']
+
+    if 'author_name_first' in fields:
+        assert text.author_name_first == fields['author_name_first']
+
+    if 'author_name_last' in fields:
+        assert text.author_name_last == fields['author_name_last']
+
+    if 'year' in fields:
+        assert text.year == fields['year']
+
+    if 'text' in fields:
+        assert fields['text'] in text.plain_text
