9f1237c941a13f6b76881fbb70519c7e5eba1332
David McClure <davidwilliammcclure@gmail.com>
2016-07-15 16:01:46 -0700

    Add a get_by() helper to the base model.

--- stacks/common/models/base.py
+++ stacks/common/models/base.py
@@ -3,9 +3,26 @@
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
+from stacks.common.singletons import session
+
 
 class Base:
+
     id = Column(Integer, primary_key=True)
+
+    @classmethod
+    def get_by(cls, **kwargs):
+
+        """
+        Get the first row that matches a set of filters.
+        """
+
+        return (
+            session
+            .query(cls)
+            .filter_by(**kwargs)
+            .first()
+        )
 
 
 Base = declarative_base(cls=Base)
