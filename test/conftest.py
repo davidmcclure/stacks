956a2ffa105eab2e88db7bfbf8a07cd384183a50
David McClure <davidwilliammcclure@gmail.com>
2016-07-15 10:34:05 -0700

    Add db_module and rq_module fixtures.

--- test/conftest.py
+++ test/conftest.py
@@ -44,8 +44,42 @@ def db():
     session.remove()
 
 
+# TODO: dry
+@pytest.yield_fixture(scope='module')
+def db_module():
+
+    """
+    Reset the testing database.
+    """
+
+    # Get a new connection.
+    engine = config.build_sqla_engine()
+    connection = engine.connect()
+
+    # Start a nested transaction.
+    transaction = connection.begin_nested()
+    session.configure(bind=connection)
+
+    yield
+
+    # Rollback changes.
+    transaction.rollback()
+    session.remove()
+
+
 @pytest.fixture
 def rq():
+
+    """
+    Clear the RQ queue.
+    """
+
+    _rq.connection.flushdb()
+
+
+# TODO: dry
+@pytest.fixture(scope='module')
+def rq_module():
 
     """
     Clear the RQ queue.
