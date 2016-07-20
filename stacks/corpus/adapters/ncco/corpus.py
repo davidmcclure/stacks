eb5063f7c0ce147e81996bbf58dc58d8ff0f1846
David McClure <davidwilliammcclure@gmail.com>
2016-07-20 10:34:56 -0700

    Move in NCCO.

---
+++ stacks/corpus/adapters/ncco/corpus.py
@@ -0,0 +1,45 @@
+
+
+import os
+
+from stacks.corpus.utils import scan_paths
+from stacks.corpus.models import Corpus as StacksCorpus
+
+from .jobs import ingest
+
+
+class Corpus:
+
+    def __init__(self, path):
+
+        """
+        Canonicalize the corpus path.
+
+        Args:
+            path (str)
+        """
+
+        self.path = os.path.abspath(path)
+
+    def text_paths(self):
+
+        """
+        Generate paths to the XML sources.
+
+        Yields: str
+        """
+
+        return scan_paths(self.path, '\.xml$')
+
+    def ingest(self):
+
+        """
+        Queue ingest jobs for each text.
+        """
+
+        corpus = StacksCorpus.replace(
+            slug='ncco',
+            name='Nineteenth Century Collections Online',
+        )
+
+        corpus.queue_ingest(ingest, self.text_paths())
