62cb7f9e846be1fceefbbff6e74258787ee77c7c
David McClure <davidwilliammcclure@gmail.com>
2016-07-19 19:30:29 -0700

    Wrap Corpus.create() in with_flush().

--- stacks/common/utils.py
+++ stacks/common/utils.py
@@ -7,6 +7,29 @@ from sqlalchemy.exc import DatabaseError
 from .singletons import session
 
 
+def with_flush(f):
+
+    """
+    Flush after a function.
+
+    Returns: func
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+
+        try:
+            res = f(*args, **kwargs)
+            session.flush()
+            return res
+
+        except DatabaseError:
+            session.rollback()
+            raise
+
+    return wrapper
+
+
 def with_commit(f):
 
     """
@@ -19,8 +42,9 @@ def with_commit(f):
     def wrapper(*args, **kwargs):
 
         try:
-            f(*args, **kwargs)
+            res = f(*args, **kwargs)
             session.commit()
+            return res
 
         except DatabaseError:
             session.rollback()
