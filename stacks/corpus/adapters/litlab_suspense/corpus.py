9b16a591d7062c2c1417e8c2fedf80b43ae5660e
David McClure <davidwilliammcclure@gmail.com>
2016-07-20 10:18:02 -0700

    Move in LitLab Suspense.

---
+++ stacks/corpus/adapters/litlab_suspense/corpus.py
@@ -0,0 +1,62 @@
+
+
+import os
+
+from stacks.corpus.utils import scan_paths
+from stacks.corpus.models import Corpus as StacksCorpus
+
+from stacks.corpus.adapters.litlab.jobs import ingest
+
+
+class Corpus:
+
+    def __init__(self, path):
+
+        """
+        Canonicalize the corpus path.
+
+        Args:
+            path (str)
+        """
+
+        self.path = os.path.abspath(path)
+
+    def text_paths(self):
+
+        """
+        Generate a path for each text directory.
+
+        Yields: str
+        """
+
+        groups = [
+            'Suspenseful',
+            'Unsuspenseful',
+        ]
+
+        # Groups
+        for group in groups:
+
+            group_dir = os.path.join(self.path, group)
+
+            # Authors
+            for author_dir in next(os.walk(group_dir))[1]:
+
+                author_path = os.path.join(group_dir, author_dir)
+
+                # Texts
+                for text_dir in next(os.walk(author_path))[1]:
+                    yield os.path.join(author_path, text_dir)
+
+    def ingest(self):
+
+        """
+        Queue ingest jobs for each text.
+        """
+
+        corpus = StacksCorpus.replace(
+            slug='litlab-suspense',
+            name='Literary Lab Suspense Corpus',
+        )
+
+        corpus.queue_ingest(ingest, self.text_paths())
