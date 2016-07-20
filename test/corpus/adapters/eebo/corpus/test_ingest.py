fa57ca723a9526f657a69303db3c4ec7f0bf79b1
David McClure <davidwilliammcclure@gmail.com>
2016-07-20 10:48:08 -0700

    Add EEBO fixtures and test harness.

---
+++ test/corpus/adapters/eebo/corpus/test_ingest.py
@@ -0,0 +1,33 @@
+
+
+import pytest
+
+from stacks.corpus.models import Corpus, Text
+
+from test.utils import read_yaml
+
+
+pytestmark = pytest.mark.usefixtures('ingest')
+
+
+cases = read_yaml(__file__, 'ingest.yml')
+
+
+@pytest.mark.parametrize('identifier,fields', cases.items())
+def test_ingest(identifier, fields):
+
+    corpus = Corpus.get_by(slug='eebo')
+
+    text = Text.get_by(corpus=corpus, identifier=identifier)
+
+    if 'title' in fields:
+        assert text.title == fields['title']
+
+    if 'author_name_full' in fields:
+        assert text.author_name_full == fields['author_name_full']
+
+    if 'year' in fields:
+        assert text.year == fields['year']
+
+    if 'text' in fields:
+        assert fields['text'] in text.plain_text
