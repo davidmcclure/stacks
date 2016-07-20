85ad2fb3bcad4b097a547e7a40819ebed06ca318
David McClure <davidwilliammcclure@gmail.com>
2016-07-20 13:55:29 -0700

    Stub in the PL text wrapper.

---
+++ stacks/corpus/adapters/price_lab/corpus.py
@@ -0,0 +1,71 @@
+
+
+import csv
+import os
+
+from stacks.corpus.models import Corpus as StacksCorpus
+
+# from .jobs import ingest
+
+
+class Corpus:
+
+    def __init__(self, path):
+
+        """
+        Canonicalize the corpus path.
+
+        Args:
+            path (str)
+        """
+
+        self.path = os.path.abspath(path)
+
+    def metadata_path(self):
+
+        """
+        Get the path to `metadata.csv`.
+
+        Returns: str
+        """
+
+        return os.path.join(self.path, 'metadata.csv')
+
+    def texts_path(self):
+
+        """
+        Get texts/ path.
+
+        Returns: str
+        """
+
+        return os.path.join(self.path, 'texts')
+
+    def metadata(self):
+
+        """
+        Generate rows from `NOVELS_METADATA.csv`.
+
+        Yields: dict
+        """
+
+        with open(self.metadata_path(), 'r') as fh:
+            yield from csv.DictReader(fh)
+
+    # def ingest(self):
+
+        # """
+        # Queue ingest jobs for each text.
+        # """
+
+        # args = [
+            # dict(corpus_path=self.path, metadata=row)
+            # for row in self.novels_metadata()
+        # ]
+
+        # corpus = StacksCorpus.replace(
+            # slug='chicago',
+            # name='Chicago Corpus',
+        # )
+
+        # corpus.queue_ingest(ingest, args)
---
+++ stacks/corpus/adapters/price_lab/text.py
@@ -0,0 +1,111 @@
+
+
+import os
+
+from slugify import slugify
+
+from stacks.corpus.utils import scan_paths
+
+
+class Text:
+
+    def __init__(self, texts_path, metadata):
+
+        """
+        Canonicalize the texts path, set the text metadata.
+
+        Args:
+            texts_path (str)
+            metadata (dict)
+        """
+
+        self.texts_path = os.path.abspath(texts_path)
+
+        self.metadata = metadata
+
+    def source_text_path(self):
+
+        """
+        Returns: str
+        """
+
+        paths = list(scan_paths(self.texts_path, '\.txt$'))
+
+        for path in paths:
+
+            fname = os.path.basename(path)
+
+            # Find path that starts with the title.
+            if fname.startswith(self.metadata['TITLE']):
+                return path
+
+    def source_text(self):
+
+        """
+        Returns: str
+        """
+
+        with open(
+            self.source_text_path(),
+            mode='r',
+            encoding='utf8',
+            errors='ignore',
+        ) as fh:
+
+            return fh.read()
+
+    def title(self):
+
+        """
+        Returns: str
+        """
+
+        return self.metadata['TITLE']
+
+    def author_name_first(self):
+
+        """
+        Returns: str
+        """
+
+        return self.metadata['AUTH_FIRST']
+
+    def author_name_last(self):
+
+        """
+        Returns: str
+        """
+
+        return self.metadata['AUTH_LAST']
+
+    def author_name_full(self):
+
+        """
+        Returns: str
+        """
+
+        return '{0}, {1}'.format(
+            self.author_name_last(),
+            self.author_name_first(),
+        )
+
+    def year(self):
+
+        """
+        Returns: int
+        """
+
+        return int(self.metadata['ORIG_PUBL_DATE'])
+
+    def identifier(self):
+
+        """
+        Make a slug from the surname + title.
+
+        Returns: str
+        """
+
+        return slugify(' '.join([
+            self.author_name_last(),
+            self.title(),
+        ]))
