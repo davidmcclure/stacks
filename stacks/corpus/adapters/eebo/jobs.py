1c558edf150409e4ccc032bac3232d84e61101df
David McClure <davidwilliammcclure@gmail.com>
2016-07-20 11:11:47 -0700

    Flesh out EEBO tests.

---
+++ stacks/corpus/adapters/eebo/jobs.py
@@ -0,0 +1,28 @@
+
+
+from stacks.common.utils import with_commit
+from stacks.corpus.models import Text as StacksText
+
+from .text import Text
+
+
+@with_commit
+def ingest(corpus_id, path):
+
+    """
+    Ingest an EEBO text.
+
+    Args:
+        path (str)
+    """
+
+    text = Text(path)
+
+    StacksText.create(
+        corpus_id=corpus_id,
+        identifier=text.identifier(),
+        title=text.title(),
+        author_name_full=text.author(),
+        year=text.year(),
+        plain_text=text.plain_text(),
+    )
