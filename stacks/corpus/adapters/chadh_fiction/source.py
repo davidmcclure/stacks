9ed5d47cf8e7a6d41f9f104d0081e88dc8ce0264
David McClure <davidwilliammcclure@gmail.com>
2016-07-20 09:36:30 -0700

    Move in CH fiction adapter.

---
+++ stacks/corpus/adapters/chadh_fiction/source.py
@@ -0,0 +1,33 @@
+
+
+import os
+
+from bs4 import BeautifulSoup
+
+from .text import Text
+
+
+class Source:
+
+    def __init__(self, path):
+
+        """
+        Parse the XML.
+
+        Args:
+            path (str): The text path.
+        """
+
+        self.path = os.path.abspath(path)
+
+        with open(self.path, 'rb') as fh:
+            self.xml = BeautifulSoup(fh, 'lxml')
+
+    def texts(self):
+
+        """
+        Yields: Text
+        """
+
+        for tree in self.xml.find_all('div0'):
+            yield Text(tree)
