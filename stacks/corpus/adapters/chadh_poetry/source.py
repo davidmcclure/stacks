7d654b2968e2af056e3c920d290766d4fb258772
David McClure <davidwilliammcclure@gmail.com>
2016-07-20 09:48:25 -0700

    Move in CH poetry.

---
+++ stacks/corpus/adapters/chadh_poetry/source.py
@@ -0,0 +1,33 @@
+
+
+import os
+
+from bs4 import BeautifulSoup
+
+from .poem import Poem
+
+
+class Source:
+
+    def __init__(self, path):
+
+        """
+        Parse the XML.
+
+        Args:
+            path (str): The text path.
+        """
+
+        self.path = os.path.abspath(path)
+
+        with open(self.path, 'rb') as fh:
+            self.xml = BeautifulSoup(fh, 'lxml')
+
+    def poems(self):
+
+        """
+        Yields: Poem
+        """
+
+        for tree in self.xml.find_all('poem'):
+            yield Poem(tree)
