5e420c60ba619cb49d22b7a7f09f45cf567f5672
David McClure <davidwilliammcclure@gmail.com>
2016-07-21 16:23:48 -0700

    Install the SASS gulp rig.

---
+++ stacks/manager/gulp/config.js
@@ -0,0 +1,14 @@
+
+
+import yargs from 'yargs';
+import loadPlugins from 'gulp-load-plugins';
+
+
+export const $ = loadPlugins();
+
+export const min = yargs.argv.min;
+
+export const paths = {
+  css: './assets/stylesheets',
+  out: './static',
+};
