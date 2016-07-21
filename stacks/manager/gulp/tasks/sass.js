f651b4e500e402d31f37dfd9038189337b0d5a01
David McClure <davidwilliammcclure@gmail.com>
2016-07-21 16:56:06 -0700

    Minify CSS with clean css.

--- stacks/manager/gulp/tasks/sass.js
+++ stacks/manager/gulp/tasks/sass.js
@@ -9,9 +9,11 @@ gulp.task('sass', () => {
 
   return gulp.src('./assets/stylesheets/export/index.scss')
 
-    .pipe($.if(!min, $.sourcemaps.init()))
+    .pipe($.sourcemaps.init())
     .pipe($.sass().on('error', $.sass.logError))
+
     .pipe($.if(!min, $.sourcemaps.write()))
+    .pipe($.if(min, $.cleanCss()))
 
     .pipe($.rename('export.css'))
     .pipe(gulp.dest('./static'))
