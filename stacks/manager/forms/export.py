f1244a018ff4ee0bdff4f30cbd4870a8a622b7e3
David McClure <davidwilliammcclure@gmail.com>
2016-07-24 16:37:34 -0700

    Coerge the corpora ids to ints.

--- stacks/manager/forms/export.py
+++ stacks/manager/forms/export.py
@@ -4,15 +4,34 @@ from flask_wtf import Form
 from wtforms import SelectMultipleField, IntegerField
 from wtforms.validators import DataRequired
 
+from stacks.corpus.models import Corpus
+
 
 class ExportForm(Form):
 
     corpora = SelectMultipleField(
+
+        label='Corpora',
+
+        choices=[
+            (c.id, c.name)
+            for c in Corpus.query.all()
+        ],
+
         validators=[DataRequired()],
+
+        coerce=int,
+
     )
 
-    min_year = IntegerField()
+    min_year = IntegerField(
+        label='Start Year',
+    )
 
-    max_year = IntegerField()
+    max_year = IntegerField(
+        label='End Year',
+    )
 
-    sample_size = IntegerField()
+    sample_size = IntegerField(
+        label='Sample Size',
+    )
