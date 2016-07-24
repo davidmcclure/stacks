76a97d5bcc3973cc76cdeb0de30385231318081c
David McClure <davidwilliammcclure@gmail.com>
2016-07-24 15:53:54 -0700

    Add 'corpora' column to Export.

--- stacks/corpus/models/export.py
+++ stacks/corpus/models/export.py
@@ -1,6 +1,7 @@
 
 
 from sqlalchemy import Column, Integer
+from sqlalchemy.dialects.postgresql import ARRAY
 
 from stacks.common.models import Base
 
@@ -9,6 +10,9 @@ class Export(Base):
 
 
     __tablename__ = 'export'
+
+    # TODO: Use join table?
+    corpora = Column(ARRAY(Integer))
 
     min_year = Column(Integer)
 
