cf50c2d13f177d193786f27847f6e5b49ea53893
David McClure <davidwilliammcclure@gmail.com>
2016-07-26 14:22:43 -0700

    Get rid of uuid on Export.

--- stacks/corpus/models/export.py
+++ stacks/corpus/models/export.py
@@ -3,19 +3,15 @@
 import uuid
 
 from sqlalchemy import Column, Integer, Boolean
-from sqlalchemy.dialects.postgresql import ARRAY, UUID
+from sqlalchemy.dialects.postgresql import ARRAY
 
 from stacks.common.models import Base
 
 
-# TODO: Rename to Query?
 class Export(Base):
 
 
     __tablename__ = 'export'
-
-    # TODO: Make this primary key?
-    uuid = Column(UUID)
 
     # TODO: Use join table?
     corpora = Column(ARRAY(Integer))
@@ -27,18 +23,3 @@ class Export(Base):
     sample_size = Column(Integer)
 
     finished = Column(Boolean, nullable=False, default=False)
-
-
-    @classmethod
-    def create(cls, **kwargs):
-
-        """
-        Set the uuid.
-
-        Returns: cls
-        """
-
-        return super().create(
-            uuid=str(uuid.uuid4()),
-            **kwargs
-        )
