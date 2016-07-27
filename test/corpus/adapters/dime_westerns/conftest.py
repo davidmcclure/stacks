ec8e2efc8f38809c47f5cca5fce6d0d434c4575f
David McClure <davidwilliammcclure@gmail.com>
2016-07-27 11:21:56 -0700

    Add Dime Western test cases.

---
+++ test/corpus/adapters/dime_westerns/conftest.py
@@ -0,0 +1,30 @@
+
+
+import pytest
+import os
+
+from rq import Worker
+
+from stacks.common.singletons import rq
+from stacks.corpus.adapters.dime_westerns.corpus import Corpus
+
+
+@pytest.fixture(scope='module')
+def ingest(db_module, rq_module):
+
+    """
+    Run ingest jobs.
+    """
+
+    path = os.path.join(
+        os.path.dirname(__file__),
+        'fixtures',
+    )
+
+    corpus = Corpus(path)
+
+    corpus.ingest()
+
+    # Process jobs.
+    worker = Worker([rq], connection=rq.connection)
+    worker.work(burst=True)
