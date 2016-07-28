9fdbdd23b4fee51105e075f1c74f8dd5da27ddf9
David McClure <davidwilliammcclure@gmail.com>
2016-07-28 14:18:58 -0700

    Stub in a Bundle class.

---
+++ stacks/corpus/bundle.py
@@ -0,0 +1,52 @@
+
+
+import os
+
+
+class Bundle:
+
+    def __init__(self, data_path, name):
+
+        """
+        Set the bundle name.
+
+        Args:
+            data_path (str)
+            name (str)
+        """
+
+        self.data_path = os.path.abspath(data_path)
+
+        self.name = name
+
+    @property
+    def bundle_path(self):
+        return os.path.join(self.data_path, self.name)
+
+    @property
+    def compressed_bundle_path(self):
+        return self.bundle_path + '.tar.gz'
+
+    @property
+    def texts_path(self):
+        return os.path.join(self.bundle_path, 'texts')
+
+    @property
+    def metadata_path(self):
+        return os.path.join(self.bundle_path, 'metadata.json')
+
+    @property
+    def manifest_path(self):
+        return os.path.join(self.bundle_path, 'manifest.json')
+
+    def add_text(self, text):
+        pass
+
+    def add_metadata(self):
+        pass
+
+    def write_manifest(self):
+        pass
+
+    def compress(self):
+        pass
