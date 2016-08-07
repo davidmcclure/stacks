1906cce3257861dbe12da466290dbfa027e2ce8e
David McClure <davidwilliammcclure@gmail.com>
2016-08-07 09:59:00 -0700

    Add a setup-osx script.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     name='stacks',
     version='0.1.0',
-    description='A corpus manager for literary scholars',
+    description='A corpus manager for the Stanford Literary Lab',
     url='https://github.com/davidmcclure/stacks',
     license='MIT',
     author='David McClure',
