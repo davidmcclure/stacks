1c9edcf565081ae8f1491fc0d36206325d66030e
David McClure <davidwilliammcclure@gmail.com>
2016-08-08 11:18:00 -0700

    Sketch in the flow in ext-gail-amfic.

--- bin/ext-gail-amfic.py
+++ bin/ext-gail-amfic.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 
+import numpy as np
+import json
+
 from mpi4py import MPI
+
+from stacks.corpus.adaptes.gail_amfic.corpus import Corpus
+from stacks.corpus.adaptes.gail_amfic.text import Text
 
 
 def ext_gail_amfic():
@@ -15,10 +21,34 @@ def ext_gail_amfic():
     size = comm.Get_size()
     rank = comm.Get_rank()
 
-    # get path list
-    # split into `size` chunks
-    # scatter
-    # write JSON files
+    segments = None
+
+    # Get JSON-encoded path segments.
+    if rank == 0:
+
+        corpus = Corpus.from_env()
+
+        paths = list(corpus.text_paths())
+
+        segments = [
+            json.dumps(list(s))
+            for s in np.array_split(paths, size)
+        ]
+
+    segment = comm.scatter(segments, root=0)
+
+    paths = json.loads(segment)
+
+    # corpus = ExtCorpus(config['data']['ext'])
+
+    for path in paths:
+
+        text = Text(path)
+
+        # corpus.add_text(dict(
+            # title=text.title(),
+            # ...
+        # ))
 
 
 if __name__ == '__main__':
