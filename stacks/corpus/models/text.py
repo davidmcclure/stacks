8f406e51f2d58f560cbccbecc7848e1edd884c62
David McClure <davidwilliammcclure@gmail.com>
2016-08-08 16:03:55 -0700

    Spike in an implementation of Text.ingest().

--- stacks/corpus/models/text.py
+++ stacks/corpus/models/text.py
@@ -1,6 +1,7 @@
 
 
-import hashlib
+import os
+import json
 
 from sqlalchemy import (
     Column,
@@ -14,6 +15,8 @@ from sqlalchemy import (
 from sqlalchemy.orm import relationship, validates
 
 from stacks.common.models import Base
+from stacks.common.singletons import session
+from stacks.corpus.utils import scan_paths
 
 
 class Text(Base):
@@ -58,3 +61,38 @@ class Text(Base):
         """
 
         return val.strip() if type(val) is str else val
+
+    @classmethod
+    def ingest(cls, ext_path, corpus):
+
+        """
+        Ingest extracted texts.
+
+        Args:
+            ext_path (str)
+            corpus (str)
+        """
+
+        corpus_dir = os.path.join(ext_path, corpus)
+
+        # Scan JSON files.
+        for path in scan_paths(corpus_dir, '\.json'):
+            with open(path, 'r') as fh:
+
+                # Read the JSON.
+                data = json.load(fh)
+
+                cls.create(
+
+                    corpus=corpus,
+                    identifier=data.get('identifier'),
+                    title=data.get('title'),
+
+                    author_name_full=data.get('author_name_full'),
+                    author_name_first=data.get('author_name_first'),
+                    author_name_last=data.get('author_name_last'),
+                    year=data.get('year'),
+
+                )
+
+        session.commit()
