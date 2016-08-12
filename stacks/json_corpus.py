b088f2630f1eee7325ed0b170ce94857986b755c
David McClure <davidwilliammcclure@gmail.com>
2016-08-12 11:58:57 -0700

    Rename ext_path() -> make_ext_path().

--- stacks/json_corpus.py
+++ stacks/json_corpus.py
@@ -33,7 +33,7 @@ class JSONCorpus:
 
         self.path = os.path.abspath(path)
 
-    def ext_path(self, corpus, identifier):
+    def make_ext_path(self, corpus, identifier):
 
         """
         Form the archive path for a text.
@@ -70,7 +70,7 @@ class JSONCorpus:
         text.validate()
 
         # Form the text path.
-        path = self.ext_path(text.corpus, text.identifier)
+        path = self.make_ext_path(text.corpus, text.identifier)
 
         # Ensure the directory exists.
         os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -90,7 +90,7 @@ class JSONCorpus:
         Returns: dict
         """
 
-        path = self.ext_path(corpus, identifier)
+        path = self.make_ext_path(corpus, identifier)
 
         return JSONText.from_bz2_json(path)
 
