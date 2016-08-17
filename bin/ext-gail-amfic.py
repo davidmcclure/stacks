22f243ff5066bcb89bc16662effa385cb21bcb8c
David McClure <davidwilliammcclure@gmail.com>
2016-08-16 18:26:10 -0700

    Move the MPI logic into ext-gail-amfic.

--- bin/ext-gail-amfic.py
+++ bin/ext-gail-amfic.py
@@ -1,37 +1,95 @@
 #!/usr/bin/env python
 
 
+import numpy as np
+import json
+
+from mpi4py import MPI
+
 from stacks.extractor import Extractor
 from stacks.raw.gail_amfic import Corpus, Text
+from stacks.ext import Corpus as ExtCorpus
 
 
-class GailAmficExtractor(Extractor):
+def ext_gail_amfic():
 
-    def args(self):
+    """
+    Gail American Fiction
+    """
 
-        """
-        Provide a list of ECCO paths.
+    comm = MPI.COMM_WORLD
 
-        Returns: list
-        """
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    segments = None
+
+    # ** Scatter path segments.
+
+    if rank == 0:
 
         corpus = Corpus.from_env()
 
-        return list(corpus.text_paths())
+        args = list(corpus.text_paths())
 
-    def flush(self, path):
+        segments = [
+            json.dumps(list(s))
+            for s in np.array_split(args, size)
+        ]
 
-        """
-        Flush a text.
+    segment = comm.scatter(segments, root=0)
 
-        Args:
-            path (str)
-        """
+    # ** Write JSON files.
 
-        text = Text(path)
+    args = json.loads(segment)
 
-        self.corpus.insert_text(text.to_ext_text())
+    print(len(args))
+
+    ext = ExtCorpus.from_env()
+
+    for i, arg in enumerate(args):
+
+        try:
+
+            text = Text(arg)
+
+            ext.insert_text(text.to_ext_text())
+
+        except Exception as e:
+            print(arg, e)
+
+        if i%100 == 0:
+            print(rank, i)
+
+
+# class GailAmficExtractor(Extractor):
+
+    # def args(self):
+
+        # """
+        # Provide a list of ECCO paths.
+
+        # Returns: list
+        # """
+
+        # corpus = Corpus.from_env()
+
+        # return list(corpus.text_paths())
+
+    # def flush(self, path):
+
+        # """
+        # Flush a text.
+
+        # Args:
+            # path (str)
+        # """
+
+        # text = Text(path)
+
+        # self.corpus.insert_text(text.to_ext_text())
 
 
 if __name__ == '__main__':
-    GailAmficExtractor()()
+    # GailAmficExtractor()()
+    ext_gail_amfic()
