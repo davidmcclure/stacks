8bf427b30467744eaa0104caadb4d32c43f0f08e
David McClure <davidwilliammcclure@gmail.com>
2016-08-20 11:15:15 -0700

    Spike in a parallel implementation of the metadata indexer.

--- bin/load-metadata.py
+++ bin/load-metadata.py
@@ -1,20 +1,75 @@
 #!/usr/bin/env python
 
 
-from stacks import config
+import numpy as np
+import json
+
+from stacks import config, session
 from stacks.metadata.models import Text, reset_db
+from stacks.utils import grouper
+
+from stacks.ext import (
+    Text as ExtText,
+    Corpus as ExtCorpus,
+)
 
 
-def load_manifest():
+def load_metadata():
 
     """
     Clear the metadata database and gather text rows.
     """
 
-    reset_db()
+    from mpi4py import MPI
 
-    Text.ingest()
+    comm = MPI.COMM_WORLD
+
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    # ** Scatter path segments.
+
+    segments = None
+
+    if rank == 0:
+
+        corpus = ExtCorpus.from_env()
+
+        paths = list(corpus.paths())
+
+        segments = [
+            json.dumps(list(s))
+            for s in np.array_split(paths, size)
+        ]
+
+    segment = comm.scatter(segments, root=0)
+
+    # ** Build text rows.
+
+    page = []
+    for path in json.loads(segment):
+
+        text = ExtText.from_bz2_json(path)
+
+        row = text.to_native('metadata')
+        row['path'] = path
+
+        page.append(row)
+
+    # ** Flush to disk.
+
+    pages = comm.gather(page, root=0)
+
+    # Flatten out the row list.
+    rows = [r for page in pages for r in page]
+
+    if rank == 0:
+
+        for group in grouper(rows, 1000):
+            session.bulk_insert_mappings(Text, group)
+
+        session.commit()
 
 
 if __name__ == '__main__':
-    load_manifest()
+    load_metadata()
