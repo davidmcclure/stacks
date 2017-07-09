92f47c4089394de908461622a99453d934c6ac73
David McClure <davidwilliammcclure@gmail.com>
2017-07-09 16:10:40 -0700

    Add a working corpus class.

--- stacks/corpus.py
+++ stacks/corpus.py
@@ -2,6 +2,9 @@
 
 import attr
 import os
+import pickle
+import bz2
+import uuid
 
 
 @attr.s
@@ -9,18 +12,46 @@ class Corpus:
 
     path = attr.ib()
 
-    def flush_row(self, row):
+    def index_rows(self, corpus, rows):
         """Dump db rows + (annotated) text.
+
+        Args:
+            corpus (str): A slug for the corpus.
+            rows (list of model instances)
         """
-        corpus = getattr(row, 'corpus', None)
-        text_hash = getattr(row, 'text_hash', None)
+        for row in rows:
+            if getattr(row, '_text', None):
+                self.write_text(row)
 
-        if not corpus or not text_hash:
-            return
+        self.pickle_rows(corpus, rows)
 
-        prefix = text_hash[:3]
-        suffix = text_hash[3:]
+    def pickle_rows(self, corpus, rows):
+        """Pickle row instances.
+        """
+        # Form the row path.
+        row_name = '{}.p'.format(str(uuid.uuid4()))
+        row_path = os.path.join(self.path, 'rows', corpus, row_name)
 
-        path = os.path.join(self.path, corpus, prefix, suffix)
+        # Create the directory.
+        os.makedirs(os.path.dirname(row_path), exist_ok=True)
 
-        return path
+        with open(row_path, 'wb') as fh:
+            pickle.dump(rows, fh)
+
+    def write_text(self, row):
+        """Write plain text + annotations.
+        """
+        prefix = row.text_hash[:3]
+        suffix = row.text_hash[3:]
+
+        # Form the file path.
+        text_path = os.path.join(
+            self.path, 'texts', row.corpus,
+            prefix, suffix, 'text.bz2',
+        )
+
+        # Create the text directory.
+        os.makedirs(os.path.dirname(text_path), exist_ok=True)
+
+        with bz2.open(text_path, 'wt') as fh:
+            print(row._text, file=fh)
