c8af8be2c6844e08113fb49c15242ef3129cbab7
David McClure <davidwilliammcclure@gmail.com>
2017-07-14 14:12:33 -0700

    Stub in EEBO model.

---
+++ stacks/models/eebo.py
@@ -0,0 +1,44 @@
+
+
+from sqlalchemy import Column, Integer, String, Date, Float
+
+from stacks.models import Base
+from .text import Text
+
+
+class EEBOText(Text, Base):
+
+    __tablename__ = 'eebo_text'
+
+    idno_dlps = Column(String, primary_key=True)
+
+    title = Column(String)
+
+    author = Column(String)
+
+    file_extent = Column(String)
+
+    file_pubplace = Column(String)
+
+    file_publisher = Column(String)
+
+    file_date = Column(String)
+
+    # TODO: Take second stc id?
+    idno_stc = Column(String)
+
+    idno_eebo_citation = Column(String)
+
+    idno_proquest = Column(String)
+
+    idno_vid = Column(String)
+
+    source_extent = Column(String)
+
+    source_pubplace = Column(String)
+
+    source_publisher = Column(String)
+
+    source_date = Column(Integer)
+
+    language = Column(String)
--- test/bin/ext_ncco/test_extract.py
+++ test/bin/ext_ncco/test_extract.py
@@ -8,42 +8,6 @@ from stacks.models import NCCOText, NCCOSubjectHead
 from test.utils import read_yaml
 
 
-# @pytest.fixture(scope='module', autouse=True)
-# def extract(mpi):
-    # call(['mpirun', 'bin/ext-ncco.py'])
-    # call(['mpirun', 'bin/load-metadata.py'])
-
-
-# cases = read_yaml(__file__, 'texts.yml')
-
-
-# @pytest.mark.parametrize('psmid,spec', cases.items())
-# def test_extract(psmid, spec, ext_corpus):
-
-    # row = NCCOText.query.get(psmid)
-
-    # # Fields
-    # for key, val in spec['fields'].items():
-        # assert getattr(row, key) == val
-
-    # # Subjects
-    # for subject in spec.get('subjects', []):
-        # for sub_field, value in subject['sub_fields'].items():
-
-            # query = NCCOSubjectHead.query.filter_by(
-                # psmid=row.psmid,
-                # type=subject['type'],
-                # sub_field=sub_field,
-                # value=value,
-            # )
-
-            # assert query.count() == 1
-
-    # # Text
-    # text = ext_corpus.load_text(row)
-    # assert spec['text'] in text
-
-
 fields = read_yaml(__file__, 'fields.yml')
 subjects = read_yaml(__file__, 'subjects.yml')
 texts = read_yaml(__file__, 'texts.yml')
