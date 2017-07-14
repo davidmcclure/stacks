1fe79a4f9c824bcaebbecdf36623d1fa69b12e22
David McClure <davidwilliammcclure@gmail.com>
2017-07-13 17:54:05 -0700

    Start to break up the BPO tests.

--- test/bin/ext_bpo/test_extract.py
+++ test/bin/ext_bpo/test_extract.py
@@ -17,19 +17,62 @@ def extract(mpi):
 cases = read_yaml(__file__, 'texts.yml')
 
 
-@pytest.mark.parametrize('record_id,spec', cases.items())
-def test_extract(record_id, spec, ext_corpus):
+# @pytest.mark.parametrize('record_id,spec', cases.items())
+# def test_extract(record_id, spec, ext_corpus):
+
+    # row = BPOArticle.query.get(record_id)
+
+    # # TODO: Parametrize everything as separate tests?
+
+    # # Fields
+    # for key, val in spec['fields'].items():
+        # assert getattr(row, key) == val
+
+    # # Flex terms
+    # for term in spec['flex_terms']:
+
+        # query = BPOFlexTerm.query.filter_by(
+            # record_id=row.record_id,
+            # **term
+        # )
+
+        # assert query.count() == 1
+
+    # # Contributors
+    # for contrib in spec['contributors']:
+
+        # query = BPOContributor.query.filter_by(
+            # record_id=row.record_id,
+            # **contrib
+        # )
+
+        # assert query.count() == 1
+
+    # # Text
+    # text = ext_corpus.load_text(row)
+    # assert spec['text'] in text
+
+
+fields = read_yaml(__file__, 'fields.yml')
+
+@pytest.mark.parametrize('record_id,fields', fields.items())
+def test_fields(record_id, fields):
 
     row = BPOArticle.query.get(record_id)
 
-    # TODO: Parametrize everything as separate tests?
-
-    # Fields
-    for key, val in spec['fields'].items():
+    for key, val in fields.items():
         assert getattr(row, key) == val
 
+
+flex_terms = read_yaml(__file__, 'flex_terms.yml')
+
+@pytest.mark.parametrize('record_id,terms', flex_terms.items())
+def test_flex_terms(record_id, terms):
+
+    row = BPOArticle.query.get(record_id)
+
     # Flex terms
-    for term in spec['flex_terms']:
+    for term in terms:
 
         query = BPOFlexTerm.query.filter_by(
             record_id=row.record_id,
@@ -37,17 +80,3 @@ def test_extract(record_id, spec, ext_corpus):
         )
 
         assert query.count() == 1
-
-    # Contributors
-    for contrib in spec['contributors']:
-
-        query = BPOContributor.query.filter_by(
-            record_id=row.record_id,
-            **contrib
-        )
-
-        assert query.count() == 1
-
-    # Text
-    text = ext_corpus.load_text(row)
-    assert spec['text'] in text
