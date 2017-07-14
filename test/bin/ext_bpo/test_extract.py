ffadcba9f6bda2bf7af62ad42fe28554dfc1080b
David McClure <davidwilliammcclure@gmail.com>
2017-07-14 13:01:52 -0700

    Finish breaking out BPO tests.

--- test/bin/ext_bpo/test_extract.py
+++ test/bin/ext_bpo/test_extract.py
@@ -8,52 +8,17 @@ from stacks.models import BPOArticle, BPOFlexTerm, BPOContributor
 from test.utils import read_yaml
 
 
+fields = read_yaml(__file__, 'fields.yml')
+texts = read_yaml(__file__, 'texts.yml')
+flex_terms = read_yaml(__file__, 'flex_terms.yml')
+contribs = read_yaml(__file__, 'contribs.yml')
+
+
 @pytest.fixture(scope='module', autouse=True)
 def extract(mpi):
     call(['mpirun', 'bin/ext-bpo.py'])
     call(['mpirun', 'bin/load-metadata.py'])
 
-
-cases = read_yaml(__file__, 'texts.yml')
-
-
-# @pytest.mark.parametrize('record_id,spec', cases.items())
-# def test_extract(record_id, spec, ext_corpus):
-
-    # row = BPOArticle.query.get(record_id)
-
-    # # TODO: Parametrize everything as separate tests?
-
-    # # Fields
-    # for key, val in spec['fields'].items():
-        # assert getattr(row, key) == val
-
-    # # Flex terms
-    # for term in spec['flex_terms']:
-
-        # query = BPOFlexTerm.query.filter_by(
-            # record_id=row.record_id,
-            # **term
-        # )
-
-        # assert query.count() == 1
-
-    # # Contributors
-    # for contrib in spec['contributors']:
-
-        # query = BPOContributor.query.filter_by(
-            # record_id=row.record_id,
-            # **contrib
-        # )
-
-        # assert query.count() == 1
-
-    # # Text
-    # text = ext_corpus.load_text(row)
-    # assert spec['text'] in text
-
-
-fields = read_yaml(__file__, 'fields.yml')
 
 @pytest.mark.parametrize('record_id,fields', fields.items())
 def test_fields(record_id, fields):
@@ -63,8 +28,6 @@ def test_fields(record_id, fields):
     for key, val in fields.items():
         assert getattr(row, key) == val
 
-
-flex_terms = read_yaml(__file__, 'flex_terms.yml')
 
 @pytest.mark.parametrize('record_id,terms', flex_terms.items())
 def test_flex_terms(record_id, terms):
@@ -81,8 +44,6 @@ def test_flex_terms(record_id, terms):
         assert query.count() == 1
 
 
-contribs = read_yaml(__file__, 'contributors.yml')
-
 @pytest.mark.parametrize('record_id,contribs', contribs.items())
 def test_contributors(record_id, contribs):
 
@@ -96,3 +57,11 @@ def test_contributors(record_id, contribs):
         )
 
         assert query.count() == 1
+
+
+@pytest.mark.parametrize('record_id,text', texts.items())
+def test_text(record_id, text, ext_corpus):
+
+    row = BPOArticle.query.get(record_id)
+
+    assert text in ext_corpus.load_text(row)
