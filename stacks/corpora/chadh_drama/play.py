799d8f05e2b189b24de52596f0b4d209d4364703
David McClure <davidwilliammcclure@gmail.com>
2017-07-17 11:31:37 -0700

    Stub in drama adapter.

--- stacks/corpora/chadh_drama/play.py
+++ stacks/corpora/chadh_drama/play.py
@@ -1,40 +1,107 @@
 
 
-from stacks.utils import get_text
+import attr
+
+from cached_property import cached_property
+
+from stacks.utils import get_text, try_or_log
+from stacks.models import ChadhDramaText
 
 
+@attr.s
 class Play:
 
-    def __init__(self, xml):
-        """Set the XML tree.
+    xml = attr.ib()
 
-        Args:
-            xml (BeautifulSoup)
-        """
-        self.xml = xml
-
-    def identifier(self):
+    @cached_property
+    def idref(self):
         """Returns: str
         """
-        return get_text(self.xml, 'idref')
+        return get_text(self.xml, 'comhd0 idref')
 
+    @try_or_log
+    def database(self):
+        """Returns: str
+        """
+        return get_text(self.xml, 'newatts attdbase')
+
+    @try_or_log
     def title(self):
         """Returns: str
         """
-        return get_text(self.xml, 'newatts atttitle')
+        return get_text(self.xml, 'somhead voltitle')
 
-    def author_full(self):
+    @try_or_log
+    def title_full(self):
         """Returns: str
         """
-        return get_text(self.xml, 'attribs attauth')
+        return get_text(self.xml, 'source pubtitle')
 
-    def year(self):
+    @try_or_log
+    def author(self):
+        """Returns: str
+        """
+        return get_text(self.xml, 'author nameinv')
+
+    @try_or_log
+    def author_id(self):
+        """Returns: int
+        """
+        return int(get_text(self.xml, 'newatts attautid'))
+
+    @try_or_log
+    def author_gender(self):
+        """Returns: str
+        """
+        return get_text(self.xml, 'attribs attgend')
+
+    @try_or_log
+    def publisher(self):
+        """Returns: str
+        """
+        return get_text(self.xml, 'source citn publ')
+
+    @try_or_log
+    def pub_date(self):
         """Returns: int
         """
         return int(get_text(self.xml, 'newatts attpubn1'))
 
+    @try_or_log
+    def pub_date2(self):
+        """Returns: int
+        """
+        return int(get_text(self.xml, 'newatts attpubn2'))
+
+    @try_or_log
+    def pub_city(self):
+        """Returns: str
+        """
+        return get_text(self.xml, 'source citn city')
+
+    @try_or_log
     def plain_text(self):
         """Returns: str
         """
         # TODO: Scrub out metadata.
         return ' '.join(self.xml.strings)
+
+    def row(self):
+        """Assemble a database row.
+
+        Returns: AmficText
+        """
+        return ChadhFictionText(
+            id=self.idref,
+            database=self.database(),
+            title=self.title(),
+            title_full=self.title_full(),
+            author=self.author(),
+            author_id=self.author_id(),
+            author_gender=self.author_gender(),
+            publisher=self.publisher(),
+            pub_date=self.pub_date(),
+            pub_date2=self.pub_date2(),
+            pub_city=self.pub_city(),
+            text=self.plain_text(),
+        )
